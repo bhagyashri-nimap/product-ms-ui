ef467e3ffc8caa58add4108a9768190332d45686
bhagyashri3 <bhagyashri@nimapinfotech.com>
2021-10-06 14:33:46 +0530

    added changes

--- product-ms-ui/src/App.vue
+++ product-ms-ui/src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <router-link to="/" class="navbar-brand">VueCrud</router-link>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link to="/tutorials" class="nav-link"
+            >CategoryList</router-link
+          >
+        </li>
+        <li class="nav-item">
+          <router-link to="/add" class="nav-link">Add</router-link>
+        </li>
+        <!-- <li class="nav-item">
+          <router-link to="/productList" class="nav-link"
+            >Product List</router-link
+          >
+        </li>
+        <li class="nav-item">
+          <router-link to="/addProduct" class="nav-link"
+            >Add Product</router-link
+          >
+        </li> -->
+      </div>
+    </nav>
+
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
